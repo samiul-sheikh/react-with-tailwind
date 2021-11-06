0b4c3816ec0203c43423cb210e094d394ffead91
samiul-sheikh <sheikh.samiul1971@gmail.com>
2021-11-07 02:21:40 +0600

    project setup ready & install tailwind, react hook form, yup

---
+++ src/Components/OtpForm/OtpForm.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const OtpForm = () => {
+    return (
+        <div className="text-center">
+            <p className="text-6xl">Hello Tailwind !</p>
+        </div>
+    );
+};
+
+export default OtpForm;
